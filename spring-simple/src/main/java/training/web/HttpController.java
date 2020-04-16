60782a6e559f849322f8cd477daebb72a6887025
igurmai <istvan.gurmai@capgemini.com>
2020-04-16 12:21:48 +0200

    adding customers-page.html

--- spring-simple/src/main/java/training/web/HttpController.java
+++ spring-simple/src/main/java/training/web/HttpController.java
@@ -1,8 +1,12 @@
 package training.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import training.backend.CustomerService;
 
 @Controller
 public class HttpController {
@@ -11,6 +15,18 @@ public class HttpController {
     @ResponseBody
     public String sayHello() {
         return "Hello World!";
+    }
+    
+    private CustomerService customerService;
+
+    public HttpController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/customers")
+    public ModelAndView listEmployees() {
+        var customers = customerService.listCustomers();
+        return new ModelAndView("customers-page", "customerList", customers);
     }
 
 }
